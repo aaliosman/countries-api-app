d830bf354e70c8ae848887be319da92c8ce4791d
aaliosman <aliosman027@gmail.com>
2022-01-30 16:01:38 +0100

    countries-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import CreateCard from './CreateCard'
+
+import { InputGroup, Button, FormControl} from 'react-bootstrap';
+
 import './App.css';
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState('all');
+ 
+  const [isLoading, setIsLoading] = useState(false);
+
+  // console.log(input);
+
+  useEffect(() => {
+    getData();
+  }, [input]);
+
+  const getData = () => {
+    if (input !== ''){
+      const all = `https://restcountries.com/v3.1/${input}`;
+      const name = `https://restcountries.com/v3.1/name/${input}`;
+      fetch(input === 'all' ? all : name)
+        .then((response) => response.json())
+        .then((json) => {
+             
+          setData(json);
+         
+          console.log(data[0]);
+  
+        }).then(() => {setInput('')})
+    }
+   
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header>
+        <h1> Countries Information</h1>
+        <input
+        placeholder="enter country name"
+          Value={input}
+          type="text"
+          onChange={(e) => {
+            // if (e.target.value === '') {
+            //   setInput('all');
+             
+            // } else {
+            //   setInput(e.target.value);
+              
+            // }
+            setInput(e.target.value);
+          }}
+        />
+        <br />
+        <br />
+        {/* <button onClick={ () =>{
+          getData()
+      
+        }
+          
+         
+        }>search</button> */}
+                      
+        </header>
+ 
+      </div>
+      <div className="card-main">
+      {data.map((it , index) => {
+      
+      return (
+        <CreateCard 
+        key= {index}
+         cont = {it}
+         input = {setInput}
+        />
+      );
+    })}
+      </div>
+    </>
   );
 }
-
-export default App;
---
+++ src/CreateCard.js
@@ -0,0 +1,109 @@
+import React, {useState} from 'react';
+import {Card} from 'react-bootstrap';
+
+const CreateCard = (props) => {
+
+    const [cardInfo, setCardInfo] = useState(false);
+    const [showMap, setShowMap] = useState(false);
+    const [CardShow, setCardShow] = useState(false);
+   
+  return (
+    <>
+      {
+       cardInfo ?
+       <div className='card-info'>
+           <img src={props.cont.flags.png} alt="" />
+           <div className="main-info">
+           <div className="country-info-left">
+            <h4> Country name : <span>{props.cont.name.common}</span>  </h4>
+            <h4> Capital : <span>{props.cont.capital}</span>  </h4>
+            <h4> Continents/Region : <span>{props.cont.continents}</span>  </h4>
+            {/* <h4> Currencies : {Object.values(props.cont.currencies.LYD)} </h4> */}
+            <h4> Languages : <span>{Object.values(props.cont.languages)}</span>  </h4>
+            <h4> Timezones : <span>{props.cont.timezones}</span>  </h4>
+        
+            <button className='map-btn'onClick={() => {setShowMap(!showMap)}}>
+                {
+                    !showMap ? 'show in maps' : 'close maps'
+                }</button>
+         
+
+           <button className='back-btn' 
+           onClick={ () => {
+               setCardInfo(!cardInfo)
+               setCardShow(false)
+               setShowMap(false)
+               }}>back</button>
+               </div>
+
+               <div className="country-info-right">
+                   <h3>Borders Countries</h3>
+                   <div className="borders">
+                   
+                      {
+                         
+                        
+                          props.cont.borders.map((neben) => {
+                            // if (props.cont.includes('borders')) {
+                             return <div className="neben"
+                             onClick={ () => {props.input(neben)}}
+                             > {neben} </div>
+                            
+                            
+                          })
+                            
+                          
+                      }
+                          {
+                            showMap &&
+                                <iframe className='iframe' src={props.cont.maps.openStreetMaps} ></iframe>
+                          }
+                   </div>
+               </div>
+               </div>
+       </div>
+       :
+       null
+   }
+
+    <Card key={props.key} style={{ width: '15rem' }} className="card"
+    onClick={() => {
+        setCardInfo(true)
+        // setCardShow(!CardShow)
+        }}>
+        
+        
+    
+    
+    <Card.Img variant="top" key={props.key} src={props.cont.flags.png} />
+    <Card.Body key={props.key}>
+      <Card.Title key={props.key}>
+       
+        <h1>
+          {props.cont.altSpellings.map((title, index) => {
+            return (
+              <>
+                <h6 key={index}> {title} </h6>
+              </>
+            );
+          })}
+        </h1>
+      </Card.Title>
+    </Card.Body>
+    {
+        CardShow ?
+        <button 
+        onClick={ () => {
+            props.input('all')
+            // setCardInfo(false)
+        }}>zukzk</button> 
+        :
+        null
+    }
+     
+  </Card>
+  </>
+  )
+};
+
+export default CreateCard;
